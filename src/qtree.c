ad29a8e12707dcc3ed0fea905d835f107cebfdb1
Joseph Wu <josephwu0507@gmail.com>
2024-11-01 15:26:49 -0400

    create quadtree

--- src/qtree.c
+++ src/qtree.c
@@ -55,9 +55,9 @@ double calcRMSE(QTNode * node, Image * image, unsigned short startRow, unsigned 
 QTNode *create_quadtree_helper(Image *image, double max_rmse, unsigned short startRow, unsigned short startCol, unsigned short width, unsigned short height){
     QTNode * node = initializeNode(startRow, startCol, width, height);
 
-    if(height == 1 && width == 1){
-        printf("single");
-    }
+    // if(height == 1 && width == 1){
+    //     printf("single");
+    // }
     
     double RMSE = calcRMSE(node, image, startRow, startCol, width, height);
 
@@ -74,9 +74,13 @@ QTNode *create_quadtree_helper(Image *image, double max_rmse, unsigned short sta
         } else if(width > 1 && height == 1){
             node->child1 = create_quadtree_helper(image, max_rmse, startRow, startCol, width/2, height);
             node->child2 = create_quadtree_helper(image, max_rmse, startRow, startCol + (width/2), (width - (width/2)), height);
+            node->child3 = NULL;
+            node->child4 = NULL;
         } else if(width == 1 && height > 1){
             node->child1 = create_quadtree_helper(image, max_rmse, startRow, startCol, width, height/2);
             node->child3 = create_quadtree_helper(image, max_rmse, startRow + (height/2), startCol, width, (height - (height/2)));
+            node->child2 = NULL;
+            node->child4 = NULL;
         }
     }
     return node;
